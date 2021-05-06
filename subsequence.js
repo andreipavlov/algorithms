2f3dcc592a6955813bb50977f30edb2a0e8b3b71
Anastasiia Soktoeva <60854679+anastasiiasok@users.noreply.github.com>
2021-05-05 20:37:00 -0400

    Update subsequence.js

--- subsequence.js
+++ subsequence.js
@@ -6,3 +6,16 @@
 // array. 
 // For instance, the numbers [1, 3, 4]form a subsequence of the array [1, 2, 3, 4] , and so do the numbers [2, 4] 
 // Note that a single number in an array and the array itself are both valid subsequences of the array
+
+function isValidSubsequence(array, sequence) {
+	let seqIdx =0;
+	for (const value of array){
+		if (seqIdx === sequence.length) break;
+		if (sequence[seqIdx] === value ) seqIdx++;
+	}
+	return seqIdx === sequence.length
+}
+console.log( isValidSubsequence([5, 1, 22, 25, 6, -1, 8, 10],
+ [5, 1, 22, 6, -1, 8, 10]))
+
+// should return true or false
