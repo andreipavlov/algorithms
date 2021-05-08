63a5f96bb72ca2c57ff09526e4e1600caad3e865
Anastasiia Soktoeva <60854679+anastasiiasok@users.noreply.github.com>
2021-05-07 20:35:59 -0400

    Create ArrayMap.js

---
+++ ArrayMap.js
@@ -0,0 +1,10 @@
+// Multiply each index in the array by 2
+
+function multiplyArray(array) {
+  
+    let result = array.map(x => x * 2);
+	
+	return result
+
+}
+console.log (multiplyArray([2,2,3,4,5]))
