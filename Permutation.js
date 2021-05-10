037debc8581957e56a22e55b35d6b53b70014308
andreipavlov <73636994+andreipavlov@users.noreply.github.com>
2021-05-10 10:59:39 -0400

    Update Permutation.js

--- Permutation.js
+++ Permutation.js
@@ -27,3 +27,23 @@ else{
 }
 }
 console.log (arePermutations("tacocat", "ttacoca"))
+
+
+//anagram
+
+function checkAnagrams(str1, str2){
+
+  var newstr1 = str1.toLowerCase().split('').sort().join();
+  var newstr2 = str2.toLowerCase().split('').sort().join();
+
+  if(newstr1 == newstr2){
+    console.log("String is Anagrams");
+  }
+  else{
+    console.log("String is Not Anagrams");
+  }
+
+}
+
+checkAnagrams("Hello", "lolHe");
+checkAnagrams("Indian", "nIndisn");
