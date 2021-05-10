2ef86f321b0bb5f37187439e8040bfd55eec6a29
Anastasiia Soktoeva <60854679+anastasiiasok@users.noreply.github.com>
2021-05-10 11:07:44 -0400

    Update SimpleReverse.js

--- SimpleReverse.js
+++ SimpleReverse.js
@@ -2,3 +2,14 @@ function reverse(s){
     return s.split("").reverse().join("");
 }
 console.log(reverse("aveotkosaiisatsana"))
+
+
+function reverseString(str) {
+    let newString = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        newString += str[i];
+    }
+    return newString;
+}
+
+console.log (reverseString('hello'))
