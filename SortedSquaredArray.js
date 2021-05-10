e580f45b800478a129b50452233cca039b22c475
Anastasiia Soktoeva <60854679+anastasiiasok@users.noreply.github.com>
2021-05-10 17:13:35 -0400

    Second solution

--- SortedSquaredArray.js
+++ SortedSquaredArray.js
@@ -12,3 +12,16 @@ let result = array.map(x => x * x);
     return result
 
   console.log (sortedSquaredArray([2,2,3,4,5]))
+  
+
+  // O(nlogn) time | O(n) space - where n is the length of the input array
+function sortedSquaredArray(array) {
+ const sortedSquares = new Array(array.length).fill(0);
+ for (let idx = 0; idx < array.length; idx++) {
+ const value = array[idx];
+ sortedSquares[idx] = value * value;
+ }
+ sortedSquares.sort((a, b) => a - b);
+ return sortedSquares;
+}
+console.log (sortedSquaredArray([20,2,30,4,5, 2,0,4,3,1]))
