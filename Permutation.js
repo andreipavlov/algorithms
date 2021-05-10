4e8e7a35d34f5fb5beaa8460c2e3b35ae272b23a
Anastasiia Soktoeva <60854679+anastasiiasok@users.noreply.github.com>
2021-05-10 14:42:38 -0400

    Update Permutation.js

--- Permutation.js
+++ Permutation.js
@@ -27,3 +27,21 @@ else{
 }
 }
 console.log (arePermutations("tacocat", "ttacoca"))
+
+
+function(s, t) {
+ if(s === t) return true
+ if(s.length !== t.length) return false
+
+ let count = {}
+
+ for(let letter of s)
+  count[letter] = (count[letter] || 0) + 1
+
+ for(let letter of t) { 
+  if(!count[letter]) return false
+  else --count[letter]
+ }
+
+ return true;
+}
